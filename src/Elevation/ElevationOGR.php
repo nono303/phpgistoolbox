5e92a57ba244d83f0f8b44f0dfd73fc38d56cbb9
nono303 <git@nono303.net>
2023-08-02 11:33:59 +0200

    getElevation: add $debug by ref refactor init of $coordsTransformer & $dataset in __construct

--- src/Elevation/ElevationOGR.php
+++ src/Elevation/ElevationOGR.php
@@ -1,50 +1,45 @@
 <?php
 	class ElevationOGR extends ElevationSource {
 		
-		public $gdalCache = [];
-		
-		private $gdalTrCache;
+		private $coordsTransformer;
 		private $noDataVal;
 		private $srcSRS; 
 		private $trgSRS;
+		private $datafile;
 		private $dataset;
-		private $gdalregisterall;
 
-		public function __construct($srcSRS, $trgSRS, $dataset,$noDataVal = -99999) {
+		public function __construct($datafile, $srcSRS = null, $trgSRS =null , $noDataVal = -99999) {
 			if(!function_exists("gdalregisterall"))
 				throw new exception("module php-ogr not installed. see https://github.com/nono303/php-ogr");
-			$this->srcSRS = $srcSRS;
 			$this->trgSRS = $trgSRS;
-			$this->dataset = $dataset;
+			$this->datafile = $datafile;
 			$this->noDataVal = $noDataVal;
-			$this->gdalregisterall = false;
-			$this->gdalTrCache = [];
+			gdalregisterall();
+			if($srcSRS == $this->trgSRS){ // no transformation needed
+				$this->srcSRS = null;
+			} elseif($srcSRS){
+				if(!$this->trgSRS)
+					throw new Exception("target SRS 'null' must be set if source SRS '".$this->srcSRS."' specified");
+				$this->srcSRS = $srcSRS;
+				$this->coordsTransformer = gdal_tr_create($this->srcSRS,$this->trgSRS);
+			}
+			$this->dataset = gdalopen($this->datafile,$this->trgSRS);
 		}
 		
-		public function getElevation($lat,$lon){
-			return $this->getElevationDetails($lat,$lon)["value"];	
+		public function getElevation($lat,$lon,&$debug = null){
+			$debug = $this->getElevationDetails($lat,$lon);
+			if($debug["value"] == $this->noDataVal)
+				throw new Exception("out of range: ".json_encode($ret));
+			return $debug["value"];	
 		}
 		
 		public function getElevationDetails($lat,$lon){
-			if(!$this->gdalregisterall){
-				gdalregisterall();
-				$this->gdalregisterall = true;
-			}
-			if($this->srcSRS && !$this->gdalCache[$this->dataset]){
-				$this->trgSRS = -1;
-				$this->gdalCache[$this->dataset] = ["GDALDataset" => gdalopen($this->dataset,$this->trgSRS), "trgSRS" => $this->trgSRS];
-				if(!$this->gdalTrCache[$this->srcSRS."-".$this->trgSRS])
-					$this->gdalTrCache[$this->srcSRS."-".$this->trgSRS] = gdal_tr_create($this->srcSRS,$this->trgSRS);
-			}
-			$ret = ["src" => "rgealti"];
 			if($this->srcSRS){
-				$ret += gdal_locationinfo($this->gdalCache[$this->dataset]["GDALDataset"],$lon,$lat,$this->gdalTrCache[$this->srcSRS."-".$this->gdalCache[$this->dataset]["trgSRS"]]);
+				$ret = gdal_locationinfo($this->dataset,$lon,$lat,$this->coordsTransformer);
 			} else {
-				$ret += gdal_locationinfo($this->gdalCache[$this->dataset]["GDALDataset"],$lon,$lat);
+				$ret = gdal_locationinfo($this->dataset,$lon,$lat);
 			}
-			$ret["raster"] += ["dataset" => $this->dataset];
-			if($ret["value"] == $this->noDataVal)
-				throw new Exception("out of range: ".json_encode($ret));
+			$ret["raster"] += ["datafile" => $this->datafile];
 			return $ret;
 		}
 	}
